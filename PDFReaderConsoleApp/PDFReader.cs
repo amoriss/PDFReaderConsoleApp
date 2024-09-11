5b75438394dc73e13e6949c07f0bed7a02e35c02
Amoriss <amoriss@protonmail.com>
2024-09-10 23:39:32 -0500

    initial commit

---
+++ PDFReaderConsoleApp/PDFReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace PDFReaderConsoleApp;
+
+public class PDFReader
+{
+    public static void PrintTextFromPDFToConsole()
+    {
+        try
+        {
+            using (PdfDocument document = PdfDocument.Open(@"Data/Simple_doc_Helvetica_Neue_Font.pdf"))
+            {
+                foreach (Page page in document.GetPages())
+                {
+                    string pageText = page.Text;
+                    foreach(Word word in page.GetWords())
+                    {
+                        Console.WriteLine(word.Text);
+                    }
+
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+     
+        }
+       
+    }
+}
