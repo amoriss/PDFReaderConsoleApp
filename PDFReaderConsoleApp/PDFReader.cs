10ca73c32ce6aa383dfcb01da1bd93056e54524e
Amoriss <amoriss@protonmail.com>
2024-09-17 14:44:57 -0500

    made ReadAllPagesFromResume, SetSkillsFromResume, and GetSkillsFromResume methods

--- PDFReaderConsoleApp/PDFReader.cs
+++ PDFReaderConsoleApp/PDFReader.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata;
+using PDFReaderConsoleApp.Models;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
 using UglyToad.PdfPig.Graphics.Operations.PathPainting;
@@ -7,20 +8,21 @@ namespace PDFReaderConsoleApp;
 
 public class PDFReader
 {
+    private static readonly string _relativePath =
+        //concatenate path
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "FakeResume_1.pdf");
+
     public static void PrintTextFromPDFToConsole()
     {
-        
-        //concatenate path
-        string relativePath =
-            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "FakeResume_1.pdf");
-        bool usesCSharp = false;
-        bool usesVisualStudio = false;
-        bool usesSql = false;
-        bool usesAspnet = false;
+       
+    }
+
+    public static void ReadAllPagesFromResume()
+    {
         try
         {
             //using statement makes sure resources are properly disposed of after use.
-            using (PdfDocument document = PdfDocument.Open(relativePath))
+            using (PdfDocument document = PdfDocument.Open(_relativePath))
             {
                 int counter = 0;
                 foreach (Page page in document.GetPages())
@@ -29,27 +31,6 @@ public class PDFReader
                     Console.WriteLine($"Reading page: {counter}.");
                     var pageText = page.Text.ToLower();
                     Console.WriteLine("------------------------");
-
-                    if (pageText.Contains("visual studio"))
-                    {
-                        usesVisualStudio = true;
-                    }
-
-                    if (pageText.Contains("c#"))
-                    {
-                        usesCSharp = true;
-                    }
-
-                    if (pageText.Contains("asp.net"))
-                    {
-                        usesAspnet = true;
-                    }
-
-                    if (pageText.Contains("sql"))
-                    {
-                        usesSql = true;
-                    }
-                    
                 }
             }
         }
@@ -57,27 +38,53 @@ public class PDFReader
         {
             Console.WriteLine(e.Message);
         }
+    }
 
-        if (usesCSharp)
+    public static void SetSkillsFromResume(string pageText, Skills skills)
+    {
+        if (pageText.Contains("visual studio"))
+        {
+            skills.UsesVisualStudio = true;
+        }
+
+        if (pageText.Contains("c#"))
+        {
+            skills.UsesCSharp = true;
+        }
+
+        if (pageText.Contains("asp.net"))
+        {
+            skills.UsesAspnet = true;
+        }
+
+        if (pageText.Contains("sql"))
+        {
+            skills.UsesSql = true;
+        }
+    }
+
+    public static void GetSkillsFromResume(Skills skills)
+    {
+        if (skills.UsesCSharp)
         {
             Console.WriteLine("Language: C#");
         }
 
-        if (usesAspnet)
+        if (skills.UsesAspnet)
         {
             Console.WriteLine("Framework: ASP.NET");
         }
 
-        if (usesVisualStudio)
+        if (skills.UsesVisualStudio)
         {
             Console.WriteLine("IDE: Visual Studio");
         }
 
-        if (usesSql)
+        if (skills.UsesSql)
         {
             Console.WriteLine("Database: SQL");
         }
-        else if(!usesCSharp && !usesAspnet && !usesVisualStudio && !usesSql)
+        else if (!skills.UsesCSharp && !skills.UsesAspnet && !skills.UsesVisualStudio && !skills.UsesSql)
         {
             Console.WriteLine("This candidate does not have tools and languages you are looking for.");
         }
